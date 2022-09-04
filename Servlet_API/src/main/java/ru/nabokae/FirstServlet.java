a502024cd38aed5b1ea7269645fcc0fad141fa9f
NabokaEA <nabokae@yandex.ru>
2022-09-05 01:22:06 +0300

    1.1 Servlet API ready

---
+++ Servlet_API/src/main/java/ru/nabokae/FirstServlet.java
@@ -0,0 +1,36 @@
+package ru.nabokae;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = "/serv1")
+public class FirstServlet implements Servlet {
+
+    private ServletConfig servletConfig;
+
+    @Override
+    public void init(ServletConfig servletConfig) throws ServletException {
+        this.servletConfig = servletConfig;
+    }
+
+    @Override
+    public ServletConfig getServletConfig() {
+        return servletConfig;
+    }
+
+    @Override
+    public void service(ServletRequest servletRequest, ServletResponse servletResponse) throws ServletException, IOException {
+        servletResponse.getWriter().println("<h1> Ответ от сервлета serv1</h1>");
+    }
+
+    @Override
+    public String getServletInfo() {
+        return null;
+    }
+
+    @Override
+    public void destroy() {
+
+    }
+}
