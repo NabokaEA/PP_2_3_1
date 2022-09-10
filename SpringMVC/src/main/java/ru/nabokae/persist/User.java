360eccc951975eba7dec08b73bec41dbf3956226
NabokaEA <nabokae@yandex.ru>
2022-09-11 00:00:30 +0300

    4.1 разделение конфигови пополнение зависимостей

--- SpringMVC/src/main/java/ru/nabokae/persist/User.java
+++ SpringMVC/src/main/java/ru/nabokae/persist/User.java
@@ -8,6 +8,7 @@ import javax.validation.constraints.Min;
 import javax.validation.constraints.NotBlank;
 
 @Entity
+@Table(name="users")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
